522d8382598cde68d9c281bcdf71474bb7453083
Segerlu <steven.segerlund@yahoo.com>
2022-09-13 17:20:33 -0400

    Makes the GUI more user friendly and appealing

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(windowWidth, windowHeight*.75);
     background(0);
     noLoop();
 
     let angleContainer = createDiv("Angle:");
     angleContainer.addClass("container");
-    angleSlider = createSlider(0, TWO_PI, PI/4, 0.01);
+    angleSlider = createSlider(0, TWO_PI, PI / 4, 0.01);
     angleSlider.addClass("sliders");
     angleContainer.child(angleSlider);
 
@@ -17,10 +17,20 @@ function setup() {
 
     let branchContainer = createDiv("Branches:");
     branchContainer.addClass("container");
-    branchSlider = createSlider(0.4, 0.8, 0.6, .01);
+    branchSlider = createSlider(0.4, 0.75, 0.6, .01);
     branchSlider.addClass("sliders");
     branchContainer.child(branchSlider);
 
+    let radioContainer = createDiv("Which way do you want to tree to lean?");
+    radioContainer.addClass("container");
+    lean = createRadio();
+    lean.option('left');
+    lean.option('center');
+    lean.option('right');
+    lean.style('width', '800px');
+    radioContainer.child(lean);
+
+    lean.input(redraw);
     branchSlider.input(redraw);
     angleSlider.input(redraw);
     sizeSlider.input(redraw);
@@ -33,23 +43,33 @@ function draw() {
     let angle = angleSlider.value();
     let offset = branchSlider.value();
     stroke(255);
-    translate(width/2, height)
+    translate(width / 2, height)
     drawLine(angle, length, offset);
 }
 
 function drawLine(angle, length, offset) {
 
-    if (length > 5) {
+    if (length > 3) {
         line(0, 0, 0, -length);
         translate(0, -length);
+
         push();
-        rotate(angle);
+        if (lean.value() === "left") {
+            rotate(angle / 2);
+        } else {
+            rotate(angle);
+        }
         drawLine(angle, length * offset, offset)
         pop();
+
         push();
-        rotate(-angle);
+        if (lean.value() === "right") {
+            rotate(-angle / 2);
+        } else {
+            rotate(-angle);
+        }
         drawLine(angle, length * offset, offset)
         pop();
-    }   
+    }
 
 }
